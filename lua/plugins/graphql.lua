07f6b80453e4a47badac86cb115abd0448f7bd29
Gianluca Klimmer <pixelmannvip@gmail.com>
2025-02-03 13:03:22 +0100

    Add graphql

---
+++ lua/plugins/graphql.lua
@@ -0,0 +1,29 @@
+return {
+  -- add cli
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "graphql-language-service-cli" })
+    end,
+  },
+
+  -- add lsp
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.graphql = opts.servers.graphql or {}
+    end,
+  },
+
+  -- add highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "graphql" })
+    end,
+  },
+}
